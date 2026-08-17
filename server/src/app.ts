d842200767c84b8ece2de521a2336106799ac6b1
buildwithzeke <ayeoluwaseyi@gmail.com>
2026-08-17 15:35:22 +0100

    set cors config to accept all origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,22 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes';
-import { errorHandler } from './middleware/errorHandler';
-import { NotFoundError } from './shared/errors/appError';
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+import { errorHandler } from "./middleware/errorHandler";
+import { NotFoundError } from "./shared/errors/appError";
 
 const app = express();
 
 // Global Middlewares
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Central Routing Hook
-app.use('/api', routes);
+app.use("/api", routes);
 
 // Handle 404/Not Found Routes
 app.use((req, _res, next) => {
-  next(new NotFoundError(`Cannot find ${req.method} ${req.originalUrl} on this server`));
+  next(
+    new NotFoundError(
+      `Cannot find ${req.method} ${req.originalUrl} on this server`,
+    ),
+  );
 });
 
 // Global Error Handler
